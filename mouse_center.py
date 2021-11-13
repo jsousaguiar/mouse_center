01182daae6d993f407b458882d4d559c90342427
Jorge Eduardo de Sousa Aguiar <sousa.aguiar@gmail.com>
2021-11-13 19:13:09 -0300

    Atualizados o requiremntes.txt e ajustado o tamanho do frame

--- mouse_center.py
+++ mouse_center.py
@@ -2,10 +2,13 @@ import pyautogui
 import tkinter as tk
 
 # Center mouse in main window
+
+
 def mouse_finder():
-    x = int(pyautogui.size().width / 2) #+ 24
-    y = int(pyautogui.size().height / 2) #+ 24
+    x = int(pyautogui.size().width / 2)  # + 24
+    y = int(pyautogui.size().height / 2)  # + 24
     pyautogui.moveTo(x, y)
+
 
 window = tk.Tk()
 window.title("Mouse Finder")
@@ -14,12 +17,11 @@ texto_orientacao = tk.Label(frame, text="Mouse successfully recovered!")
 botao = tk.Button(frame, text="Close", command=window.destroy)
 texto_vazio = tk.Label(frame)
 
-frame.pack(padx=10, pady=10)
-texto_orientacao.pack(padx=10,pady=(30, 0))
-botao.pack(padx=20,pady=(10, 0))
-texto_vazio.pack(padx=0,pady=(10, 10))
+frame.pack(padx=40, pady=10)
+texto_orientacao.pack(padx=10, pady=(30, 0))
+botao.pack(padx=20, pady=(10, 0))
+texto_vazio.pack(padx=0, pady=(10, 10))
 mouse_finder()
 
-window.eval('tk::PlaceWindow . center')
+window.eval("tk::PlaceWindow . center")
 window.mainloop()
-
